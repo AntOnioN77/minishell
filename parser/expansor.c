6fed0f2aae5dc7b92f6c77c91a21cbc59256fd35
Jose luis Perez rabasot <jperez-r@c2r14s4.42madrid.com>
2025-01-26 20:24:47 +0100

    rebase

--- parser/expansor.c
+++ parser/expansor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expansor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antofern <antofern@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: jperez-r <jperez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 15:45:35 by antofern          #+#    #+#             */
-/*   Updated: 2025/01/25 20:19:44 by antofern         ###   ########.fr       */
+/*   Updated: 2025/01/26 20:24:23 by jperez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	add_pathname(char **cmd,  t_garbage *garbage, char *envp[])
 		//	si la encuentra:
 		//	-libera garbaje (aun no se como va a saber si cmd fue o no fue expandida )
 		//	-costruye path/name
-
+	return(0);
 }
 */
 
