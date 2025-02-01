39b182fc832c4cb3aa622b16a46598611f56e5be
Jose luis Perez rabasot <jperez-r@c2r9s6.42madrid.com>
2025-02-01 19:42:15 +0100

    signals

--- signal.c
+++ signal.c
@@ -13,13 +13,13 @@ void handle_sigint(int signal)
 		rl_redisplay();
     }
 }
-void handle_sigquit(int signal)
+/*void handle_sigquit(int signal)
 {
 	(void)signal;
     rl_on_new_line();
     rl_replace_line(rl_line_buffer, 0);
     rl_redisplay();
-}
+}*/
 
 /**
  * Configura las señales para CTRL-C (SIGINT) y CTRL-\ (SIGQUIT)
@@ -28,10 +28,4 @@ void	signalConf(void)
 {
 	signal(SIGINT, handle_sigint);
 	signal(SIGQUIT, SIG_IGN);
-	
-	//Esto funciona para silenciar completamente Ctrl-\, pero está fuera de norma
-	/*sigset_t block;
-    sigemptyset(&block);
-    sigaddset(&block, SIGQUIT);
-    sigprocmask(SIG_BLOCK, &block, NULL);*/
 }
