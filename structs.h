9df273cae37452098b31b8c6b5a021969c36e0c3
Anto <ant@onion.com>
2025-03-26 00:33:59 +0100

    sin comentarios casi

--- structs.h
+++ structs.h
@@ -6,40 +6,29 @@
 /*   By: antofern <antofern@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 01:43:53 by antofern          #+#    #+#             */
-/*   Updated: 2025/03/25 23:59:43 by antofern         ###   ########.fr       */
+/*   Updated: 2025/03/26 00:31:26 by antofern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /*_________________________TREE_STRUCTS______________________________________*/
-/*Este struct no tiene un caso de uso real, funciona como interface. Los
-nodos del arbol son en realidad t_line t_pipe t_redir t_task.
-El uso de una interface nos permite pasar cualquiera de estos tipos como
-argumento a una función. El elemento type, permite a esa función determinar
-que tipo de dato ha recibido
-en realidad.*/
 typedef struct s_tree
 {
 	enum e_nodes	type;
 	char			*line;
-	char			*line_extra;//Solo en caso de "ls|(vacio)" line_extra !=NULL
+	char			*line_extra;
 }	t_tree;
 
-/*La linea recibida como task se parsea de izquuierda a derecha, sucesivas
-redirecciones se sobreescriben. de manera que hay un solo t_redir por
-cada t_task. si no se encuentra ninguna redireccion los enum e_symbols
-pemanecen "none"
-*/
 typedef struct s_redir
 {
 	enum e_nodes	type;
-	enum e_symbols	insymbol;//< o <<
-	char			*infoo;// será un archivo para <, O un separator para <<
-	enum e_symbols	outsymbol;//>> o >
+	enum e_symbols	insymbol;
+	char			*infoo;
+	enum e_symbols	outsymbol;
 	char			*outfile;
 	char			*tmp_file;
-	t_errors		error; //Error causado durante la creación de tmp_file
+	t_errors		error;
 }	t_redir;
 
 typedef struct s_garbage
@@ -52,8 +41,8 @@ typedef struct s_garbage
 typedef struct s_task
 {
 	enum e_nodes	type;
-	char			*line;	//solo el primer nodo del arbol contendrá line!=NULL
-	char			*line_extra;//Solo en caso de "ls|(vacio)" line_extra !=NULL
+	char			*line;
+	char			*line_extra;
 	t_redir			redir;
 	char			*cmd;
 	char			**argv;
@@ -61,18 +50,11 @@ typedef struct s_task
 	int				pid;
 }	t_task;
 
-//si left o rigth fuesen NULL indica error de reserva de memoria, hay que
-//liverar el arbol entero y lanzar error.
-//Un pipe tal que "ls|   (nada)" generaria: pipe->rigth->task, y task tendria
-//sus elementos vacios lo cual no es un error, este ejemplo en concreto deja
-//a bash pendiente de entrada.
-//left siempre debe contener un elemento task, y no puede contener otra cosa
-//rigth puede contener otro elemento pipe o un elemento task
 typedef struct s_pipe
 {
 	enum e_nodes	type;
-	char			*line;	//solo el primer nodo del arbol contendrá line!=NULL
-	char			*line_extra;//Solo en caso de "ls|(vacio)" line_extra !=NULL
+	char			*line;
+	char			*line_extra;
 	t_task			*left;
 	t_tree			*rigth;
 }	t_pipe;
@@ -80,14 +62,7 @@ typedef struct s_pipe
 /*______________________________Other_Structs____________________________*/
 typedef struct s_environ
 {
-	// gestion de variables de netorno:
 	char	**envp;
-	int		next;//posicion no inicializada (null) donde podemos escribir la siguiente variable global
-	int		alloced; //numero total de posiciones allocadas, de manera que si next+1 < alloced, no sera necesario reallocar memoria
-	// gestion de variables locales:
-	char	**local;
-	int		locnext;
-	int		localloced;
-
-//	sighandler_t *sigint_handler;
+	int		next;
+	int		alloced;
 }	t_environ;
